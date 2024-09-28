b3bb2da7587504c8c2b523d9498d5a4160986c6c
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-09-28 16:58:50 +0530

    day 45

--- practice.sql
+++ practice.sql
@@ -63,3 +63,12 @@ FROM Employee
 FULL OUTER Join Bonus
 On Employee.empId = Bonus.empId
 Where Bonus.bonus < 1000 OR Bonus.bonus IS NULL;
+
+-- Problem 12
+SELECT s.student_id, s.student_name, sub.subject_name, COALESCE(COUNT(e.student_id), 0) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e
+ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
