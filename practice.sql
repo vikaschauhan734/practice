182f33098a56f2bb43bf0af1a27339fcadcdcb97
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-10-05 10:38:43 +0530

    day 52

--- practice.sql
+++ practice.sql
@@ -116,3 +116,9 @@ SELECT contest_id, ROUND(COUNT(user_id)*100/(SELECT COUNT(*) AS total_users FROM
 FROM Register
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id ASC;
+
+-- Problem 19
+SELECT query_name, ROUND(AVG(rating/position), 2) AS quality,
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
