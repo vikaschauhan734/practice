16915a152fe49a41beb8f78988d55baabe74d913
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-10-06 11:37:01 +0530

    day 53

--- practice.sql
+++ practice.sql
@@ -122,3 +122,10 @@ SELECT query_name, ROUND(AVG(rating/position), 2) AS quality,
     ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS poor_query_percentage
 FROM Queries
 GROUP BY query_name;
+
+-- Problem 20
+SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month, country, COUNT(id) AS trans_count,
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count, SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY DATE_FORMAT(trans_date, '%Y-%m'), country;
