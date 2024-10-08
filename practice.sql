44274ae47f12ca20bb5b44c4368b32c3dea4e107
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-10-08 16:37:44 +0530

    day 55

--- practice.sql
+++ practice.sql
@@ -143,3 +143,17 @@ FirstOrdersDetails AS (SELECT D.delivery_id, D.customer_id, D.order_date, D.cust
 )
 SELECT ROUND(100.0 * SUM(is_immediate) / COUNT(*), 2) AS immediate_percentage
 FROM FirstOrdersDetails;
+
+-- Problem 22
+WITH FirstLogin AS (SELECT player_id, MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+),
+NextDayLogin AS (SELECT a.player_id
+    FROM Activity a
+    JOIN FirstLogin f ON a.player_id = f.player_id
+    WHERE a.event_date = DATE_ADD(f.first_login, INTERVAL 1 DAY)
+)
+SELECT ROUND(COUNT(DISTINCT n.player_id) / COUNT(DISTINCT f.player_id), 2) AS fraction
+FROM FirstLogin f
+LEFT JOIN NextDayLogin n ON f.player_id = n.player_id;
