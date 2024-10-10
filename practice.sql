5fef79f7dc7ec239dc7bcb649b5e315ff9e80c9b
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-10-10 21:21:35 +0530

    day 57

--- practice.sql
+++ practice.sql
@@ -163,3 +163,10 @@ LEFT JOIN NextDayLogin n ON f.player_id = n.player_id;
 SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt
 FROM Teacher
 GROUP BY teacher_id;
+
+-- Problem 24
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date
+ORDER BY activity_date;
