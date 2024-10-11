dd051bdd01e7076829e569fbaa2954c6a1a0d8df
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-10-11 16:41:44 +0530

    day 58

--- practice.sql
+++ practice.sql
@@ -170,3 +170,11 @@ FROM Activity
 WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
 GROUP BY activity_date
 ORDER BY activity_date;
+
+-- Problem 25
+SELECT s.product_id, s.year AS first_year, s.quantity, s.price
+FROM Sales s
+JOIN (SELECT product_id, MIN(year) AS first_year
+    FROM Sales GROUP BY product_id
+) AS first_sales
+ON s.product_id = first_sales.product_id AND s.year = first_sales.first_year;
