9966362d18071e912dd5b0546458a67447467706
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-10-12 14:10:02 +0530

    day 59

--- practice.sql
+++ practice.sql
@@ -178,3 +178,9 @@ JOIN (SELECT product_id, MIN(year) AS first_year
     FROM Sales GROUP BY product_id
 ) AS first_sales
 ON s.product_id = first_sales.product_id AND s.year = first_sales.first_year;
+
+-- Problem 26
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
