d62fd9c99c6cafa58f80cda46483759c4f0c41a4
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-10-16 13:59:38 +0530

    day 62

--- practice.sql
+++ practice.sql
@@ -196,3 +196,11 @@ SELECT customer_id
 FROM Customer
 GROUP BY customer_id
 HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product);
+
+-- Problem 29
+SELECT e.employee_id, e.name, COUNT(r.employee_id) AS reports_count, ROUND(AVG(r.age), 0) AS average_age
+FROM Employees e
+JOIN Employees r 
+ON e.employee_id = r.reports_to
+GROUP BY e.employee_id, e.name
+ORDER BY e.employee_id;
