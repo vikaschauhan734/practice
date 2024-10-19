b64ffe99efc584683279dae680e1d64c81ea5f91
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-10-19 20:46:49 +0530

    day 64

--- practice.sql
+++ practice.sql
@@ -215,3 +215,11 @@ WHERE primary_flag = 'Y'
        GROUP BY employee_id
        HAVING COUNT(*) = 1
    );
+
+-- Problem 31
+SELECT x, y, z,
+       CASE 
+           WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+           ELSE 'No'
+       END AS triangle
+FROM Triangle;
