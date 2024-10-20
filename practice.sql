67a59ba2f62dcc82cdc2d4bf8873fc94bc8fad63
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-10-20 16:43:22 +0530

    day 65

--- practice.sql
+++ practice.sql
@@ -223,3 +223,13 @@ SELECT x, y, z,
            ELSE 'No'
        END AS triangle
 FROM Triangle;
+
+-- Problem 32
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT num,
+           LAG(num) OVER (ORDER BY id) AS prev_num,
+           LEAD(num) OVER (ORDER BY id) AS next_num
+    FROM Logs
+) subquery
+WHERE num = prev_num AND num = next_num;
