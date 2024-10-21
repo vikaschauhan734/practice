22cf6ec51c4e0286a54bb93156bac85a0b5655b7
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-10-21 16:40:45 +0530

    day 66

--- practice.sql
+++ practice.sql
@@ -233,3 +233,17 @@ FROM (
     FROM Logs
 ) subquery
 WHERE num = prev_num AND num = next_num;
+
+-- Problem 33
+SELECT 
+    product_id, 
+    COALESCE((
+        SELECT new_price 
+        FROM Products p2 
+        WHERE p1.product_id = p2.product_id 
+        AND p2.change_date <= '2019-08-16' 
+        ORDER BY p2.change_date DESC 
+        LIMIT 1
+    ), 10) AS price
+FROM 
+    (SELECT DISTINCT product_id FROM Products) p1;
