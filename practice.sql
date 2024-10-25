c795050547dfde55147ec54982683f368025c09a
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-10-25 21:56:06 +0530

    day 70

--- practice.sql
+++ practice.sql
@@ -282,3 +282,13 @@ WHERE e.salary < 30000
   AND m.employee_id IS NULL
 ORDER BY e.employee_id;
 
+-- Problem 37
+SELECT 
+    CASE 
+        WHEN id % 2 = 1 AND id + 1 <= (SELECT MAX(id) FROM Seat) THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id
+    END AS id, student
+FROM Seat
+ORDER BY id;
+
