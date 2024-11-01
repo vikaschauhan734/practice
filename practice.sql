597339d1e9205cfab59f5dab2b1366a907bf4ed3
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-11-01 15:41:38 +0530

    day 73

--- practice.sql
+++ practice.sql
@@ -342,3 +342,9 @@ HAVING
   COUNT(*) = 7
 ORDER BY
   c.visited_on ASC;
+
+-- Problem 40
+SELECT user_id,
+CONCAT(UPPER(SUBSTRING(name, 1, 1)),LOWER(SUBSTRING(name, 2))) AS name
+FROM Users
+ORDER BY user_id;
