636db7075793627f9616e08e2d679a22539f95b5
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-11-06 14:02:13 +0530

    day 77

--- practice.sql
+++ practice.sql
@@ -372,3 +372,10 @@ SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
 FROM Insurance
 WHERE tiv_2015 IN (SELECT tiv_2015 FROM Insurance GROUP BY tiv_2015 HAVING COUNT(*) > 1)
 AND (lat, lon) IN (SELECT lat, lon FROM Insurance GROUP BY lat, lon HAVING COUNT(*) = 1);
+
+-- Problem 44
+DELETE p1
+FROM Person p1
+JOIN Person p2
+ON p1.email = p2.email
+AND p1.id > p2.id;
