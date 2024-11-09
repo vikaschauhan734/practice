b95943c26e7688a5659895a69b1bcaa739c35f54
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-11-09 09:16:13 +0530

    day 80

--- practice.sql
+++ practice.sql
@@ -395,3 +395,13 @@ SELECT
      FROM Employee 
      ORDER BY salary DESC 
      LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+
+-- Problem 47
+WITH empdep AS (SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary, 
+    DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS salary_rank
+FROM Employee e
+LEFT JOIN Department d
+ON e.departmentId = d.id)
+SELECT Department, Employee, Salary
+FROM empdep
+WHERE salary_rank <= 3;
