b530d48f9926b0308c4197164d413900031af78b
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-11-10 13:36:54 +0530

    day 81

--- practice.sql
+++ practice.sql
@@ -405,3 +405,10 @@ ON e.departmentId = d.id)
 SELECT Department, Employee, Salary
 FROM empdep
 WHERE salary_rank <= 3;
+
+-- Problem 48
+SELECT sell_date, COUNT(DISTINCT(product)) AS num_sold,
+GROUP_CONCAT(DISTINCT(product) ORDER BY product ASC) AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date;
